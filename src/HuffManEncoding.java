af75fc6b46cba4c6fd9cc30b01895da49df2c677
Ritesh Shah <arits.king74@gmail.com>
2023-11-07 22:58:34 -0600

    Application to encode a set of given words using a pre-determied bits aasignment using Huffman Coding

---
+++ src/HuffManEncoding.java
@@ -0,0 +1,27 @@
+public class HuffManEncoding {
+    public String encoder(String num){
+        StringBuilder sb= new StringBuilder();
+        for(int i=0; i<=num.length()-1;i++){
+            if(num.charAt(i)=='P'){
+                sb.append("01");
+            }else if(num.charAt(i)=='M'){
+                sb.append("001");
+            }else if(num.charAt(i)=='R'){
+                sb.append("100");
+            }else if(num.charAt(i)=='Q'){
+                sb.append("000");
+            }else if(num.charAt(i)=='S'){
+                sb.append("101");
+            }else if(num.charAt(i)=='E'){
+                sb.append("11");
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        HuffManEncoding hme= new HuffManEncoding();
+       System.out.println(hme.encoder("MEMRSQMPPQSEEEQRSMEE"));
+        //hme.encoder("10100011101010111010010110001111010100101011010");
+    }
+}
